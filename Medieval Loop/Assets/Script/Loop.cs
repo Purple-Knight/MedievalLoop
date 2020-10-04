b957ca814127b69500b18f239dff2d055511990b
nicolas <nico.valadou@orange.fr>
2020-10-04 11:14:03 +0200

    monde1

---
+++ Medieval Loop/Assets/Script/Loop.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loop : MonoBehaviour
+{
+    public int nLoop = 0;
+    private CamController cam;
+    public GameObject[] boucle;
+    public int nboucle = 1;
+
+    public string monde;
+
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CamController>();
+        boucle[0].SetActive(true);
+    }
+
+    void Update()
+    {
+        if (nLoop < boucle.Length)
+        {
+            if (nboucle == nLoop)
+            {
+                nboucle++;
+                boucle[nLoop].SetActive(true);
+                boucle[nLoop - 1].SetActive(false);
+            }
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            nLoop++;
+            if (nLoop == boucle.Length)
+            {
+                SceneManager.LoadScene(monde);
+            }
+            else
+            {
+                col.transform.position = GameObject.FindGameObjectWithTag("Spawn").transform.position;
+                cam.boss = false;
+            }
+
+
+        }
+    }
+}
