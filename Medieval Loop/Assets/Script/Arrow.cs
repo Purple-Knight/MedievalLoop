71c0dc39d0c88720e666e414752914a449e14cb8
Thomas <thomas.takagi@edu.devinci.fr>
2020-10-04 17:05:54 +0200

    Add Scene Thomas + Crossbow

---
+++ Medieval Loop/Assets/Script/Arrow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    /*public float speed; 
+
+        private Transform player; 
+        private Vector2 target; 
+
+
+        void Start() 
+        { 
+            player = GameObject.FindGameObjectWithTag("Player").transform; 
+
+            target = new Vector2(player.position.x, player.position.y); 
+        }*/
+
+    void Update()
+    {
+        /*transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime); 
+ 
+        if(transform.position.x == target.x && transform.position.y == target.y) 
+        { 
+            DestroyArrow(); 
+        }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D Col)
+    {
+        if (Col.collider.gameObject.tag == "Player")
+        {
+            DestroyArrow();
+
+        }
+        else
+        {
+            DestroyArrow();
+        }
+    }
+
+    void DestroyArrow()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Medieval Loop/Assets/Script/Crossbow.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossbow : MonoBehaviour
+{
+    public float range;
+    public Transform target;
+    bool detected = false;
+    Vector2 direction;
+
+
+    public GameObject bullet;
+    public Transform firePoint;
+    public float fireRate;
+    float timeBtwShoot = 0;
+    public float force;
+
+
+    private void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Vector2 targetPos = target.position;
+
+        direction = targetPos - (Vector2)transform.position;
+
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, direction, range);
+
+        if (rayInfo)
+        {
+            if (rayInfo.collider.gameObject.tag == "Player")
+            {
+                if (detected == false)
+                {
+                    detected = true;
+                }
+            }
+
+            else
+            {
+                if (detected == true)
+                {
+                    detected = false;
+                }
+            }
+        }
+
+        if (detected)
+        {
+            transform.up = direction;
+            if (Time.time > timeBtwShoot)
+            {
+                timeBtwShoot = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject ArrowIns = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        ArrowIns.GetComponent<Rigidbody2D>().AddForce(direction * force);
+    }
+
+    /*private float timeBtwShoots; 
+    public float startTimeBtwShoot; 
+    public float startShootingDistance; 
+    public GameObject arrow; 
+ 
+    public Transform player; 
+    private Vector2 target; 
+    public Rigidbody2D rb; 
+ 
+ 
+ 
+    void Start() 
+    { 
+        timeBtwShoots = startTimeBtwShoot; 
+        player = GameObject.FindGameObjectWithTag("Player").transform; 
+        target = new Vector2(player.position.x, player.position.y); 
+ 
+    } 
+ 
+    void Update() 
+    { 
+        if (Vector2.Distance(transform.position, player.position) < startShootingDistance) 
+        { 
+            if (timeBtwShoots <= 0) 
+            { 
+                Instantiate(arrow, transform.position, Quaternion.identity); 
+                timeBtwShoots = startTimeBtwShoot; 
+            } 
+            else 
+            { 
+                timeBtwShoots -= Time.deltaTime; 
+            } 
+        } 
+    }*/
+}
