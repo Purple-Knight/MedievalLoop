d7e72b9fe95990ded96f7d52750ae321a06280d4
nicolas <nico.valadou@orange.fr>
2020-10-05 17:30:49 +0200

    Monde 3

---
+++ Medieval Loop/Assets/Script/CageGoToStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageGoToStart : MonoBehaviour
+{
+    public GameObject cage;
+    public GameObject startPoint;
+    private void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            cage.transform.position = GameObject.FindGameObjectWithTag("Start").transform.position; 
+        }
+    }
+}
